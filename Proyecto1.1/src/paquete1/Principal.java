a9aee289e2f1600e4a754de505b926c06bb971e7
usuario <usuario@DESKTOP-59MKDT2>
2023-03-09 13:49:51 +0100

    Añado Bucle

--- Proyecto1.1/src/paquete1/Principal.java
+++ Proyecto1.1/src/paquete1/Principal.java
@@ -5,5 +5,9 @@ public class Principal {
 		System.out.println("Estamos muy contentos porque es jueves");
 		System.out.println("Mañana estaremos más contentos porque será viernes");
 		System.out.println("El lunes ya no estaremos tan contentos");
+		
+		for(int i=0;i<5;i++) {
+			System.out.println("HOLA");
+		}
 	}
 }
