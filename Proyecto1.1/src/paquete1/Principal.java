922fffca45946a238eac0416ad3e029e2ef937a9
usuario <usuario@DESKTOP-59MKDT2>
2023-03-09 13:43:41 +0100

    Tercer Mensaje

--- Proyecto1.1/src/paquete1/Principal.java
+++ Proyecto1.1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[]arg) {
 		System.out.println("Estamos muy contentos porque es jueves");
 		System.out.println("Mañana estaremos más contentos porque será viernes");
+		System.out.println("El lunes ya no estaremos tan contentos");
 	}
 }
